7bbd15adfd9214cff37cdb94f02173d0dedc7546
Joan Pato <joan.pato@enti.cat>
2020-07-30 21:53:27 +0200

    Basic hp script, separate scene for playerscript

---
+++ New Unity Project/Assets/Scripts/BasicEntScripts/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntity : MonoBehaviour
+{
+    // Contains basic monobehaviour data an ID for the entity and hp with death system
+    public int ID = 0;
+    public int hp;
+    public virtual void ModHealth(int modValue, BaseEntity damagerBase)
+    {
+
+    }
+
+    public virtual void Death()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/BasicEntScripts/BaseStaticVars.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStaticVars : MonoBehaviour
+{
+    //Used at start of program, every player and enemy gets assigned a unique ID
+    public static int entID = 1;
+}
